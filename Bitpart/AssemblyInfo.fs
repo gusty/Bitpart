ba044207613a98b45633b86108c02ca657bbd433
gmpl <gustavo.perezleon@gmail.com>
2015-02-11 23:40:15 +0100

    + AssemblyInfo for DLL and Server. + nuspec for Server.

---
+++ Bitpart/AssemblyInfo.fs
@@ -0,0 +1,11 @@
+﻿module AssemblyInfo
+
+open System.Reflection
+open System.Runtime.CompilerServices
+open System.Runtime.InteropServices
+
+[<assembly: System.Runtime.CompilerServices.InternalsVisibleTo("Bitpart.Test")>]
+[<assembly: AssemblyVersion("1.0.0.0")>]
+[<assembly: AssemblyFileVersion("1.0.0.0")>]
+[<assembly: AssemblyInformationalVersion("1.0.0")>]
+do()
